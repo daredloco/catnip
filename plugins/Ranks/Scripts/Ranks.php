f4167a0ec5f6093357c4ad98cfc4e25b029fd451
daredloco <deadlybeatz@hotmail.com>
2021-06-25 15:14:15 -0300

    Updated Plugins\Ranks\Scripts\Ranks

--- plugins/Ranks/Scripts/Ranks.php
+++ plugins/Ranks/Scripts/Ranks.php
@@ -40,5 +40,37 @@ class Ranks{
         }
         return false;
     }
+
+    public static function HasHigherScore($user, $score)
+    {
+        if(is_null($user->rank))
+        {
+            return false;
+        }
+        $userrank = Rank::find($user->rank);
+        if($userrank->score < $score)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static function UserHasRank($rank)
+    {
+        $user = \Plugins\Auth\Scripts\Auth::user();
+        return self::HasRank($user, $rank);
+    }
+
+    public static function UserHasHigherRank($rank)
+    {
+        $user = \Plugins\Auth\Scripts\Auth::user();
+        return self::HasHigherRank($user, $rank);
+    }
+
+    public static function UserHasHigherScore($score)
+    {
+        $user = \Plugins\Auth\Scripts\Auth::user();
+        return self::HasHigherScore($user, $score);
+    }
 }
 ?>
