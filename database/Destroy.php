ceae78caaa748903dbaffb22bd3f103108f7ab51
daredloco <deadlybeatz@hotmail.com>
2021-06-25 13:16:46 -0300

    Added foreign key check disabler to Database\Destroy

--- database/Destroy.php
+++ database/Destroy.php
@@ -4,9 +4,10 @@ namespace Database;
 class Destroy{
     public static function Do()
     {
+        require_once dirname(__DIR__).'/config.php';
+        \Catnip\Database::ForeignCheck(false); //Deactivates the foreign key check temporarily
         foreach (glob('database/Tables/*.php') as $file)
         {
-            require_once dirname(__DIR__).'/config.php';
             require_once $file;
             // get the file name of the current file without the extension
             // which is essentially the class name
@@ -17,6 +18,7 @@ class Destroy{
                 $table::destroy();
             }
         }
+        \Catnip\Database::ForeignCheck(true); //Reactivates the foreign key check
     }
 }
 ?>
