2b50da4788b01c08e185cc5acc8d08e440b85d33
daredloco <deadlybeatz@hotmail.com>
2021-07-28 09:03:23 -0300

    Update Str.php

--- catnip/Utils/Str.php
+++ catnip/Utils/Str.php
@@ -5,13 +5,13 @@ class Str
 {
     public static function StartsWith($needle, $haystack)
     {
-		if ($needle != '' && strpos($haystack, $needle) === 0) return true;
-		return false;
+      if ($needle != '' && strpos($haystack, $needle) === 0) return true;
+      return false;
     }
 
     public static function EndsWith($needle, $haystack)
     {
-		if ((string) $needle === substr($haystack, -strlen($needle))) return true;
-		return false;
+      if ((string) $needle === substr($haystack, -strlen($needle))) return true;
+      return false;
     }
 }
